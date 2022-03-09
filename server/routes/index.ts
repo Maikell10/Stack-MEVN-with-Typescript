9fac88ae81f6364c6f444ca97fd5a78d486d7247
Maikell <maikell.ods10@gmail.com>
2022-03-09 00:07:17 -0400

    Project Created & Server

---
+++ server/routes/index.ts
@@ -0,0 +1,59 @@
+import { Router } from "express";
+import Task from "../models/task";
+
+const router = Router();
+
+router.get("/task", async (req, res) => {
+    const tasks = await Task.find();
+    res.send(tasks);
+});
+
+router.post("/task", async (req, res) => {
+    const { title, description } = req.body;
+
+    const task = new Task({ title, description });
+    await task.save();
+
+    res.json(task);
+});
+
+router.get("/task/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const task = await Task.findById(id);
+
+        if (!task) return res.status(404).json({ message: "Task not found" });
+
+        res.json(task);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+});
+
+router.put("/task/:id", async (req, res) => {
+    const { title, description } = req.body;
+    const { id } = req.params;
+
+    const updatedtTask = await Task.findByIdAndUpdate(
+        id,
+        { title, description },
+        { new: true }
+    );
+
+    res.json(updatedtTask);
+});
+
+router.delete("/task/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const task = await Task.findByIdAndDelete(id);
+
+        if (!task) return res.status(404).json({ message: "Task not found" });
+
+        res.json(task);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+});
+
+export default router;
